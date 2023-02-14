bfca4e958145a1aa1d16fa4950bba07ce3701026
Tomás <tcl218@alumnos.unican.es>
2023-02-14 10:56:03 +0100

    Primer Commit

---
+++ redes.py
@@ -0,0 +1,14 @@
+import math
+import torch
+import torch.nn as nn
+import random
+import scipy
+import scipy.cluster
+from scipy import stats
+import numpy as np
+from numpy import array
+import matplotlib.pyplot as plot
+
+var=1
+print(var)
+print("lol")
